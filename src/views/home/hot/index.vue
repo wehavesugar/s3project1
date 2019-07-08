dfb8ef70a399822dc36748554496e891a26f8595
Venus0510 <1290116100@qq.com>
2019-07-08 10:17:46 +0800

    a

---
+++ src/views/home/hot/index.vue
@@ -0,0 +1,492 @@
+<template>
+  <div id="wrap">
+    <header class="header">
+      <a href="#" class="back"></a>
+      <ul class="show_option">
+        <li class="opt_active">
+          <a href="#">
+            <span>正在热映</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span>即将上映</span>
+          </a>
+        </li>
+      </ul>
+    </header>
+    <div class="movie_search">
+      <div class="cityshow">
+        <b>北京</b>
+        <i></i>
+      </div>
+      <p class="search">
+        <span>影院/影片/影人，任你搜</span>
+      </p>
+    </div>
+    <div class="content">
+      <ul>
+        <li>
+          <div class="movie_img">
+            <a href="#">
+              <img src="../../../../public/img/get2.jpg" />
+              <i class="i_hot"></i>
+            </a>
+          </div>
+          <div class="movie_con">
+            <dl>
+              <dd>
+                <h3 class="movie_name">蜘蛛侠：英雄远征</h3>
+                <i class="m_score">8.1</i>
+              </dd>
+              <dt>
+                <p class="movie_detail">
+                  <i class="i_dot"></i>
+                  <span>蜘蛛侠劫后重生决战千里</span>
+                </p>
+                <div class="info">
+                  <i class="info_3D"></i>
+                  <b class="info_screen"></b>
+                </div>
+              </dt>
+            </dl>
+            <div class="fuc">
+              <i>107家影院上映1246场</i>
+              <a href="#">购票</a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="movie_img">
+            <a href="#">
+              <img src="../../../../public/img/get.jpg" />
+              <i></i>
+            </a>
+          </div>
+          <div class="movie_con">
+            <dl>
+              <dd>
+                <h3 class="movie_name">千与千寻</h3>
+                <i class="m_score">8.1</i>
+              </dd>
+              <dt>
+                <p class="movie_detail">
+                  <i class="i_dot"></i>
+                  <span>宫崎骏获奥斯卡之作</span>
+                </p>
+                <div class="info">
+                  <i class="info_3D"></i>
+                  <b class="info_screen"></b>
+                </div>
+              </dt>
+            </dl>
+            <div class="fuc">
+              <i>107家影院上映1246场</i>
+              <a href="#">购票</a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="movie_img">
+            <a href="#">
+              <img src="../../../../public/img/get2.jpg" />
+              <i class="i_hot"></i>
+            </a>
+          </div>
+          <div class="movie_con">
+            <dl>
+              <dd>
+                <h3 class="movie_name">蜘蛛侠：英雄远征</h3>
+                <i class="m_score">8.1</i>
+              </dd>
+              <dt>
+                <p class="movie_detail">
+                  <i class="i_dot"></i>
+                  <span>蜘蛛侠劫后重生决战千里</span>
+                </p>
+                <div class="info">
+                  <i class="info_3D"></i>
+                  <b class="info_screen"></b>
+                </div>
+              </dt>
+            </dl>
+            <div class="fuc">
+              <i>107家影院上映1246场</i>
+              <a href="#">购票</a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="movie_img">
+            <a href="#">
+              <img src="../../../../public/img/get.jpg" />
+              <i></i>
+            </a>
+          </div>
+          <div class="movie_con">
+            <dl>
+              <dd>
+                <h3 class="movie_name">千与千寻</h3>
+                <i class="m_score">8.1</i>
+              </dd>
+              <dt>
+                <p class="movie_detail">
+                  <i class="i_dot"></i>
+                  <span>宫崎骏获奥斯卡之作</span>
+                </p>
+                <div class="info">
+                  <i class="info_3D"></i>
+                  <b class="info_screen"></b>
+                </div>
+              </dt>
+            </dl>
+            <div class="fuc">
+              <i>107家影院上映1246场</i>
+              <a href="#">购票</a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="movie_img">
+            <a href="#">
+              <img src="../../../../public/img/get2.jpg" />
+              <i class="i_hot"></i>
+            </a>
+          </div>
+          <div class="movie_con">
+            <dl>
+              <dd>
+                <h3 class="movie_name">蜘蛛侠：英雄远征</h3>
+                <i class="m_score">8.1</i>
+              </dd>
+              <dt>
+                <p class="movie_detail">
+                  <i class="i_dot"></i>
+                  <span>蜘蛛侠劫后重生决战千里</span>
+                </p>
+                <div class="info">
+                  <i class="info_3D"></i>
+                  <b class="info_screen"></b>
+                </div>
+              </dt>
+            </dl>
+            <div class="fuc">
+              <i>107家影院上映1246场</i>
+              <a href="#">购票</a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="movie_img">
+            <a href="#">
+              <img src="../../../../public/img/get.jpg" />
+              <i></i>
+            </a>
+          </div>
+          <div class="movie_con">
+            <dl>
+              <dd>
+                <h3 class="movie_name">千与千寻</h3>
+                <i class="m_score">8.1</i>
+              </dd>
+              <dt>
+                <p class="movie_detail">
+                  <i class="i_dot"></i>
+                  <span>宫崎骏获奥斯卡之作</span>
+                </p>
+                <div class="info">
+                  <i class="info_3D"></i>
+                  <b class="info_screen"></b>
+                </div>
+              </dt>
+            </dl>
+            <div class="fuc">
+              <i>107家影院上映1246场</i>
+              <a href="#">购票</a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="movie_img">
+            <a href="#">
+              <img src="../../../../public/img/get2.jpg" />
+              <i class="i_hot"></i>
+            </a>
+          </div>
+          <div class="movie_con">
+            <dl>
+              <dd>
+                <h3 class="movie_name">蜘蛛侠：英雄远征</h3>
+                <i class="m_score">8.1</i>
+              </dd>
+              <dt>
+                <p class="movie_detail">
+                  <i class="i_dot"></i>
+                  <span>蜘蛛侠劫后重生决战千里</span>
+                </p>
+                <div class="info">
+                  <i class="info_3D"></i>
+                  <b class="info_screen"></b>
+                </div>
+              </dt>
+            </dl>
+            <div class="fuc">
+              <i>107家影院上映1246场</i>
+              <a href="#">购票</a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="movie_img">
+            <a href="#">
+              <img src="../../../../public/img/get.jpg" />
+              <i></i>
+            </a>
+          </div>
+          <div class="movie_con">
+            <dl>
+              <dd>
+                <h3 class="movie_name">千与千寻</h3>
+                <i class="m_score">8.1</i>
+              </dd>
+              <dt>
+                <p class="movie_detail">
+                  <i class="i_dot"></i>
+                  <span>宫崎骏获奥斯卡之作</span>
+                </p>
+                <div class="info">
+                  <i class="info_3D"></i>
+                  <b class="info_screen"></b>
+                </div>
+              </dt>
+            </dl>
+            <div class="fuc">
+              <i>107家影院上映1246场</i>
+              <a href="#">购票</a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hot"
+};
+</script>
+
+<style scoped>
+.header {
+  width: 100%;
+  height: 1.056rem;
+  background: #1c2635;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0 0.5rem;
+}
+.back {
+  display: block;
+  width: 1.056rem;
+  height: 1.056rem;
+  position: absolute;
+  top: 1rem;
+  left: 0.1rem;
+  color: #fff;
+  text-align: center;
+  background: url(../../../../public/img/h_btn_back.png) no-repeat 0.3rem center;
+}
+.show_option {
+  float: left;
+  width: 5.16rem;
+  height: 0.6756rem;
+  background: #0d121a;
+  border-radius: 0.3378rem;
+  text-align: center;
+}
+.show_option > li {
+  width: 2.5622rem;
+  height: 0.6356rem;
+  border-radius: 0.3178rem;
+  float: left;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.show_option > li > a {
+  display: block;
+}
+.show_option > li > a > span {
+  color: #999;
+  font-size: 0.28rem;
+}
+.opt_active {
+  background: #1c2635;
+}
+.opt_active a {
+  color: #fff;
+}
+.movie_search {
+  background: #eee;
+  width: 100%;
+  height: 1.238rem;
+  padding: 0.19rem 0.36rem;
+  border-bottom: 1px solid #999;
+}
+.cityshow {
+  float: left;
+  margin-right: 0.2rem;
+  height: 3.5em;
+}
+.cityshow b {
+  font-size: 0.32rem;
+  font-weight: normal;
+  display: block;
+  float: left;
+  margin-top: 0.2rem;
+}
+.cityshow i {
+  display: block;
+  width: 1.2rem;
+  height: 0.82rem;
+  background: url(../../../../public/img/i_city.png) no-repeat 0.8rem 0.4rem;
+}
+.search {
+  float: left;
+  width: 5rem;
+  height: 0.7876rem;
+  font-size: 0.24rem;
+  line-height: 0.7876rem;
+  padding: 0 0.5rem 0 0.95rem;
+  border: 1px solid #777;
+  border-radius: 0.18rem;
+  position: relative;
+  background: #fff url(../../../../public/img/search_ico_01.png) no-repeat
+    0.25rem 0.15rem;
+  background-size: 0.5rem 0.5rem;
+}
+.search span {
+  color: #777;
+  font-size: 0.32rem;
+}
+.content {
+  width: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 0.3514rem;
+}
+.content ul {
+  overflow: hidden;
+  width: 100%;
+}
+.content ul li {
+  overflow: hidden;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 0.2rem 0 0.2rem 0;
+}
+.content ul li .movie_img {
+  width: 1.52rem;
+  height: 2rem;
+  margin-right: 0.2rem;
+  position: relative;
+}
+.movie_img .i_hot {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: inline-block;
+  width: 0.73rem;
+  height: 0.73rem;
+  background: url(../../../../public/img/i_hot.png) no-repeat 0 0;
+}
+.content ul li .movie_con {
+  width: 5.28rem;
+  height: 2rem;
+}
+.movie_con dd {
+  display: flex;
+  width: 100%;
+  justify-content: flex-start;
+}
+.movie_con .movie_name {
+  font-size: 0.36rem;
+  font-weight: 900;
+  margin-right: 0.2rem;
+}
+.movie_con .m_score {
+  display: inline-block;
+  width: 0.5rem;
+  height: 0.5rem;
+  line-height: 0.5rem;
+  text-align: center;
+  background: #659d0e;
+  color: #fff;
+}
+.movie_con dt {
+  width: 100%;
+}
+.movie_detail {
+  padding: 0.1rem 0;
+}
+.movie_detail .i_dot {
+  display: inline-block;
+  width: 0.3rem;
+  height: 0.25rem;
+  background: url(../../../../public/img/i_dot_01.png) no-repeat 0 0;
+  background-size: cover;
+  margin: 0 0;
+}
+.movie_detail span {
+  color: #659d0e;
+  font-size: 0.28rem;
+}
+.info {
+  height: 0.3rem;
+}
+.info_3D {
+  display: inline-block;
+  background: url(../../../../public/img/ia_cine_05.png) no-repeat 0 0;
+  width: 0.4rem;
+  height: 0.3rem;
+}
+.info_screen {
+  display: inline-block;
+  background: url(../../../../public/img/ia_cine_15.png) no-repeat 0 0;
+  width: 1rem;
+  height: 0.3rem;
+}
+.fuc {
+  width: 100%;
+  height: 0.82rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.fuc i {
+  height: 0.8rems;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.26rem;
+  color: #999;
+}
+.fuc a {
+  width: 1.6rem;
+  height: 0.6rem;
+  background: #ff8600;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.4rem;
+  color: #fff;
+  font-size: 0.3rem;
+}
+</style>
